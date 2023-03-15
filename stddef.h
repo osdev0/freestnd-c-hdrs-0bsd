1b0f0ce419cf276f494bac5f476eed9425989550
mintsuki <mintsuki@protonmail.com>
2023-03-15 21:20:25 +0100

    stddef.h: Changes for C++

--- stddef.h
+++ stddef.h
@@ -3,13 +3,20 @@
 
 typedef __SIZE_TYPE__ size_t;
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
+
+#ifndef __cplusplus
 typedef __WCHAR_TYPE__ wchar_t;
+#endif
 
 #ifdef NULL
 #undef NULL
 #endif
 
-#define NULL ((void *)0)
+#ifndef __cplusplus
+#  define NULL ((void *)0)
+#else
+#  define NULL 0
+#endif
 
 #define offsetof(s, m) __builtin_offsetof(s, m)
 
