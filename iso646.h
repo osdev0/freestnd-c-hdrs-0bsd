e5667ae492cc241459a212bafdad4c855e778c6b
mintsuki <mintsuki@protonmail.com>
2023-04-25 23:19:25 +0200

    Misc updates to iso646.h to improve resiliency

--- iso646.h
+++ iso646.h
@@ -1,16 +1,31 @@
 #ifndef __FSTD_HDRS_ISO646_H
 #define __FSTD_HDRS_ISO646_H 1
 
+#ifndef __cplusplus
+
+#undef and
 #define and &&
+#undef and_eq
 #define and_eq &=
+#undef bitand
 #define bitand &
+#undef bitor
 #define bitor |
+#undef compl
 #define compl ~
+#undef not
 #define not !
+#undef not_eq
 #define not_eq !=
+#undef or
 #define or ||
+#undef or_eq
 #define or_eq |=
+#undef xor
 #define xor ^
+#undef xor_eq
 #define xor_eq ^=
+
+#endif
 
 #endif
