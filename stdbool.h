7e14ea5ae2bea6687b138d246e5fc5179e44dca0
mintsuki <mintsuki@protonmail.com>
2023-04-25 22:25:16 +0200

    Misc updates to stdbool.h to improve resiliency

--- stdbool.h
+++ stdbool.h
@@ -7,12 +7,16 @@
 #define __FSTD_HDRS_STDBOOL_H_MACROS 1
 
 #ifndef __cplusplus
+#  undef bool
 #  define bool _Bool
 
+#  undef true
 #  define true 1
+#  undef false
 #  define false 0
 #endif
 
+#undef __bool_true_false_are_defined
 #define __bool_true_false_are_defined 1
 
 #endif
